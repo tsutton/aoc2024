4bff6a99461648b384155bc72a9beaf342160be9
Taylor Sutton <taylor.sutton@clever.com>
2024-12-03 11:07:51 -0800

    Ridiculously slow Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -23,3 +23,73 @@ pub fn part1() -> i64 {
         })
         .sum()
 }
+
+struct Parser<'input> {
+    input: &'input str,
+}
+
+impl Iterator for Parser<'_> {
+    type Item = Instruction;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // println!("{}", self.input);
+        if self.input.is_empty() {
+            None
+        } else if self.input.starts_with("do()") {
+            self.input = &self.input["do()".len()..];
+            Some(Instruction::Do)
+        } else if self.input.starts_with("don't()") {
+            self.input = &self.input["don't()".len()..];
+            Some(Instruction::Dont)
+        } else {
+            let muls_regex = Regex::new(r#"^mul\((\d+),(\d+)\)"#).unwrap();
+            if let Some(capture) = muls_regex.captures(self.input) {
+                self.input = &self.input[capture.len()..];
+                Some(Instruction::Mul(
+                    capture.get(1).unwrap().as_str().parse::<i64>().unwrap(),
+                    capture.get(2).unwrap().as_str().parse::<i64>().unwrap(),
+                ))
+            } else {
+                self.input = &self.input[1..];
+                self.next()
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Instruction {
+    Do,
+    Dont,
+    Mul(i64, i64),
+}
+
+struct Interpreter {
+    enabled: bool,
+}
+
+pub fn part2() -> i64 {
+    // let input = EXAMPLE;
+    // let input = r#"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"#;
+    let input = &read_input();
+    let parser = Parser { input };
+    let mut interpreter = Interpreter { enabled: true };
+    let mut ans = 0;
+    for instr in parser {
+        println!("{:?}", instr);
+        match instr {
+            Instruction::Do => {
+                interpreter.enabled = true;
+            }
+            Instruction::Dont => {
+                interpreter.enabled = false;
+            }
+            Instruction::Mul(a, b) => {
+                if interpreter.enabled {
+                    ans += a * b;
+                }
+            }
+        }
+    }
+    ans
+}
